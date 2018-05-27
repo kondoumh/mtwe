a1431eb8c3ca5f01c277a7d1e900abe4af6b4def
kondoumh <kondoumh@gmail.com>
2018-05-27 18:14:47 +0900

    refactor.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const electron = require('electron')
+onload = () => {
+    const webview = document.getElementById('webview');
+    webview.addEventListener('new-window', function (e) {
+        electron.shell.openExternal(e.url);
+    });
+}
