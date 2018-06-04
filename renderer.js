03634e757e053f6d24ba3835bc3ff57f35763b9b
kondoumh <kondoumh@gmail.com>
2018-06-05 00:05:34 +0900

    format

--- renderer.js
+++ renderer.js
@@ -1,45 +1,45 @@
-const electron = require('electron')
+const electron = require("electron");
 onload = () => {
-    const webview = document.getElementById('webview');
-    webview.addEventListener('new-window', (e) => {
-        electron.shell.openExternal(e.url);
-    });
-}
+  const webview = document.getElementById("webview");
+  webview.addEventListener("new-window", e => {
+    electron.shell.openExternal(e.url);
+  });
+};
 
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require("electron");
 
-const ElectronSearchText = require('electron-search-text');
+const ElectronSearchText = require("electron-search-text");
 const searcher = new ElectronSearchText({
-  target: '#webview',
-  input: '.search-input',
-  count: '.search-count',
-  box: '.search-box',
-  visibleClass: '.state-visible'
+  target: "#webview",
+  input: ".search-input",
+  count: ".search-count",
+  box: ".search-box",
+  visibleClass: ".state-visible"
 });
 
-const container = document.getElementById('main');
+const container = document.getElementById("main");
 
-searcher.on('did-finish-hide', () => {
-  container.className = 'webview-container';
+searcher.on("did-finish-hide", () => {
+  container.className = "webview-container";
 });
 
-searcher.on('did-finish-show', () => {
-  container.className = 'webview-container-search';
+searcher.on("did-finish-show", () => {
+  container.className = "webview-container-search";
 });
 
-ipcRenderer.on('toggleSearch', () => {
-  searcher.emit('toggle');
+ipcRenderer.on("toggleSearch", () => {
+  searcher.emit("toggle");
 });
 
-const webview = document.getElementById('webview');
+const webview = document.getElementById("webview");
 
-ipcRenderer.on('goBack', () => {
+ipcRenderer.on("goBack", () => {
   if (webview.canGoBack()) {
     webview.goBack();
   }
 });
 
-ipcRenderer.on('goForward', () => {
+ipcRenderer.on("goForward", () => {
   if (webview.canGoForward()) {
     webview.goForward();
   }
